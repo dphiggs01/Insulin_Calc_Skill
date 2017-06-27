ed7a7a8a921c691c1474051b97f0b0c226fcfbca
Dan Higgins <daniel.higgins@yahoo.com>
2017-06-27 10:46:27 -0400

    refactor to add session.attributes property

--- diabetes_dialog.py
+++ diabetes_dialog.py
@@ -10,32 +10,26 @@ logger = logging.getLogger()
 
 
 class DiabetesDialog(DefaultDialog):
-
     def new_session_started(self, method_name=None):
         logger.debug("**************** entering DiabetesDialog.new_session_started")
-        # If we do not have a time adj offset see if we can derive one from timezone
-        time_adj = self.session.get_attribute(['time_adj'])
-        if time_adj is None:
-            timezone = self.session.get_attribute(['time_zone'])
-            # If we do not have a timezone see if we have access to the amazon profile to get one
-            if timezone is None:
-                access_token = self.session.access_token()
-                if access_token is not None:
-                    amazon_profile =  AmazonProfile(access_token)
-                    zip_code = amazon_profile.get_zip_code()
-                    zip_code_db = ZipcodeDB()
-                    timezone = zip_code_db.get_timezone_for_zip_code(zip_code)
-                    self.session.put_attribute('time_zone', timezone)
 
-            if timezone is not None:
-                time_adj = TimeOfDay.time_adj_given_tz(timezone)
+        if not self.session.attribute_exists('time_adj'):
+            # If we do not have a time adj offset see if we can derive one from timezone
+            if not self.session.attribute_exists('time_zone') and self.session.access_token is not None:
+                # If we do not have a timezone see if we have access to the amazon profile to get one
+                amazon_profile = AmazonProfile(self.session.access_token)
+                zip_code = amazon_profile.get_zip_code()
+                zip_code_db = ZipcodeDB()
+                timezone = zip_code_db.get_timezone_for_zip_code(zip_code)
+                self.session.attributes['time_zone'] = timezone
+
+            if self.session.attribute_exists('time_zone'):
+                time_adj = TimeOfDay.time_adj_given_tz(self.session.attributes['time_zone'])
                 if time_adj is not None:
-                    self.session.put_attribute('time_adj', time_adj)
-
+                    self.session.attributes['time_adj'] = time_adj
 
     def launch_request(self, method_name=None):
         logger.debug("**************** entering DiabetesDialog.launch_request")
-
         return self.execute_method('welcome_request')
 
     def blood_glucose_correction(self, method_name=None):
@@ -68,16 +62,16 @@ class DiabetesDialog(DefaultDialog):
         return Reply.build(reply_intent_dict, self.session)
 
     def calc_blood_glucose_correction(self):
-        current_bg_level = self.session.get_attribute(['current_bg_level'])
-        target_bg_level = self.session.get_attribute(['target_bg_level_' + self.day_night_prefix()])
-        correction_factor = self.session.get_attribute(['correction_factor_' + self.day_night_prefix()])
+        current_bg_level = self.session.attributes['current_bg_level']
+        target_bg_level = self.session.attributes['target_bg_level_' + self.day_night_prefix()]
+        correction_factor = self.session.attributes['correction_factor_' + self.day_night_prefix()]
         correction = int(round((float(current_bg_level) - float(target_bg_level)) / float(correction_factor), 0))
-        time_adj = self.session.get_attribute(['time_adj'])
-        self.session.put_attribute("current_time", TimeOfDay.current_time(time_adj))
-        self.session.put_attribute("day_night", self.day_night_prefix())
-        self.session.put_attribute("target_bg_level", target_bg_level)
-        self.session.put_attribute("correction_factor", correction_factor)
-        self.session.put_attribute("correction", correction)
+        time_adj = self.session.attributes['time_adj']
+        self.session.attributes['current_time'] = TimeOfDay.current_time(time_adj)
+        self.session.attributes['day_night'] = self.day_night_prefix()
+        self.session.attributes['target_bg_level'] = target_bg_level
+        self.session.attributes['correction_factor'] = correction_factor
+        self.session.attributes['correction'] = correction
         return correction
 
     def insulin_for_carb_consumption(self, method_name=None):
@@ -108,12 +102,12 @@ class DiabetesDialog(DefaultDialog):
         return Reply.build(reply_intent_dict, self.session)
 
     def calc_insulin_for_carb_consumption(self):
-        amt_of_carbs = self.session.get_attribute(['amt_of_carbs'])
-        carb_ratio = self.session.get_attribute(['carb_ratio_' + self.mealtime_prefix()])
+        amt_of_carbs = self.session.attributes['amt_of_carbs']
+        carb_ratio = self.session.attributes['carb_ratio_' + self.mealtime_prefix()]
         correction = int(round(float(amt_of_carbs) / float(carb_ratio), 0))
-        self.session.put_attribute("mealtime", self.mealtime_prefix())
-        self.session.put_attribute("carb_ratio", carb_ratio)
-        self.session.put_attribute("correction", correction)
+        self.session.attributes['mealtime'] = self.mealtime_prefix()
+        self.session.attributes['carb_ratio'] = carb_ratio
+        self.session.attributes['correction'] = correction
         return correction
 
     def bg_correction_with_carbs(self, method_name=None):
@@ -125,7 +119,7 @@ class DiabetesDialog(DefaultDialog):
             return self.handle_session_end_confused()
 
         # 2. See if we got any slots filled
-        self._event.slot_data_to_session_attributes()
+        self.event.slot_data_to_session_attributes()
 
         required_fields = ['time_adj', 'amt_of_carbs', 'current_bg_level',
                            'target_bg_level_' + self.day_night_prefix(),
@@ -142,7 +136,7 @@ class DiabetesDialog(DefaultDialog):
         correction = carb_correction
         if bg_correction > 0:
             correction += bg_correction
-        self.session.put_attribute("correction", correction)
+        self.session.attributes['correction'] = correction
 
         dose_no_dose = 'dose'
         if correction <= 0:
@@ -186,7 +180,7 @@ class DiabetesDialog(DefaultDialog):
         if requested_value == "agree":
             logger.debug('AGREED to AGREEMENT')
             date_str = datetime.date.today().strftime("%B %d, %Y")
-            self.session.put_attribute('terms_of_use', date_str)
+            self.session.attributes['terms_of_use'] = date_str
             self.session.save()
             self.pop_established_dialog()
             established_dialog = self.peek_established_dialog()
@@ -213,7 +207,7 @@ class DiabetesDialog(DefaultDialog):
 
         logger.debug(" time={} time_am_pm={} adj={}".format(requested_value, time_am_pm, time_adj))
         if time_adj is not None:
-            self.session.put_attribute('time_adj', str(time_adj))
+            self.session.attributes['time_adj'] = str(time_adj)
             self.session.save()
             self.pop_established_dialog()
             established_dialog = self.peek_established_dialog()
@@ -242,18 +236,18 @@ class DiabetesDialog(DefaultDialog):
 
     def handle_session_end_confused(self, method_name=None):
         logger.debug('**************** entering DiabetesDialog.handle_session_end_confused')
-        logger.debug("session={}".format(self.session.attributes()))
+        logger.debug("attributes={}".format(self.session.attributes))
         logger.debug("request={}".format(self.event.request))
         # can we re_prompt?
-        retry_attempted = self.session.get_attribute(["retry_attempted"])
-        if retry_attempted is None:
+        if not self.session.attribute_exists('retry_attempted'):
             prompt_dict = {"speech_out_text": "Could you please repeat or say help.",
-                          "should_end_session": False}
-            requested_value_nm = self.session.get_attribute(['requested_value_nm'])
-            if requested_value_nm is not None:
+                           "should_end_session": False}
+
+            if self.session.attribute_exists('requested_value_nm'):
+                requested_value_nm = self.session.attributes['requested_value_nm']
                 prompt_dict = self.get_re_prompt_for_slot_data(requested_value_nm)
 
-            self.session.put_attribute("retry_attempted", True)
+            self.session.attributes['retry_attempted'] = True
             return Reply.build(prompt_dict, self.session)
         else:
             # we are done
@@ -274,16 +268,11 @@ class DiabetesDialog(DefaultDialog):
     def required_fields_process(self, required_fields):
         reply_dict = None
         for key in required_fields:
-            value = self.session.get_attribute([key])
-            logger.debug("      key   {}".format(key))
-            logger.debug("      value {}".format(value))
-
-            # 4. Request data if needed
-            if value is None:
-                logger.debug("      none condition key={}".format(key))
+            if not self.session.attribute_exists(key):
+                logger.debug("      key not found for key={}".format(key))
                 expected_intent = self.get_expected_intent_for_data(key)
                 self.push_established_dialog(expected_intent)
-                self.session.put_attribute('requested_value_nm', key)
+                self.session.attributes['requested_value_nm'] = key
 
                 reply_slot_dict = self.get_slot_data_details(key)
                 return Reply.build(reply_slot_dict, self.session)
@@ -291,14 +280,14 @@ class DiabetesDialog(DefaultDialog):
         return reply_dict
 
     def day_night_prefix(self):
-        time_adj = self.session.get_attribute(['time_adj'])
+        time_adj = self.session.attributes['time_adj']
         prefix = 'night'
         if TimeOfDay.day_night(time_adj) == TimeOfDay.Daytime:
             prefix = 'day'
         return prefix
 
     def mealtime_prefix(self):
-        time_adj = self.session.get_attribute(['time_adj'])
+        time_adj = self.session.attributes['time_adj']
         if TimeOfDay.meal_time(time_adj) == TimeOfDay.Breakfast:
             prefix = 'breakfast'
         elif TimeOfDay.meal_time(time_adj) == TimeOfDay.Lunch:
